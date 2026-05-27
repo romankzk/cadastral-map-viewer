fde3c0e3fb90765cc45b74989997722113124eff
Roman Kozak <romankozak97.ua@gmail.com>
2026-05-28 00:42:17 +0300

    feat: add Perehnoiv parcels data

--- src/pages/PerehnoivMap.tsx
+++ src/pages/PerehnoivMap.tsx
@@ -35,6 +35,10 @@ export default function PerehnoivMap() {
                     isCollapsed: isCollapsed,
                     onCollapseChange: handleCollapsedChange
                 }}
+                tileLayer={{
+                    name: t('perehnoivMap.map.layerControl.cadastral'),
+                    url: "https://romankzk.github.io/map-tiles-perehnoiv/tiles/{z}/{x}/{y}.png"
+                }}
             />
 
             {/* Owner Details Modal */}
